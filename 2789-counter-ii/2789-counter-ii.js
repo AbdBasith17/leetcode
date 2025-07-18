4eef0fd79264d728c4a38735e21434c534e18763
AbdBasith17 <abzit377@gmail.com>
2025-07-18 11:15:52 +0530

    Time: 43 ms (82.13%), Space: 58.1 MB (5.47%) - LeetHub

--- 2789-counter-ii/2789-counter-ii.js
+++ 2789-counter-ii/2789-counter-ii.js
@@ -17,9 +17,3 @@ var createCounter = function(init) {
     
 };
 
-/**
- * const counter = createCounter(5)
- * counter.increment(); // 6
- * counter.reset(); // 5
- * counter.decrement(); // 4
- */
