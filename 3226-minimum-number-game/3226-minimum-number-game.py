b9b27c1aa27fea08c3e9645e0330da5fd8227726
AbdBasith17 <abzit377@gmail.com>
2025-10-08 19:49:52 +0530

    Time: 3 ms (61.92%), Space: 12.5 MB (51.49%) - LeetHub

---
+++ 3226-minimum-number-game/3226-minimum-number-game.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def numberGame(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums.sort()  
+        rev = []
+
+        for i in range(0, len(nums) - 1, 2):
+            alice = nums[i]
+            bob = nums[i+1]
+        
+            rev.append(bob)
+            rev.append(alice)
+
+        return rev
