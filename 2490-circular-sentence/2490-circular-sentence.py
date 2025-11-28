e283a9c44e77fcfa900a2702a644617877d3fe32
AbdBasith17 <abzit377@gmail.com>
2025-11-28 10:18:32 +0530

    Time: 0 ms (100%), Space: 12.4 MB (84.21%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isCircularSentence(self, sentence):
+        """
+        :type sentence: str
+        :rtype: bool
+        """
+        a = list(sentence.split())
+        for i in range(len(a)-1):
+            if  (a[i][-1]) != (a[i+1][0]):
+                return False
+        
+        if (a[-1][-1]) != (a[0][0]):
+            return False
+
+        return True     
