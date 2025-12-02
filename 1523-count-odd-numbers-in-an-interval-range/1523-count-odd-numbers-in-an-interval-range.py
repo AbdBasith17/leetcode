afeeda9833ac32f200631f58537a7f82e5efc385
AbdBasith17 <abzit377@gmail.com>
2025-12-02 13:28:27 +0530

    Time: 11 ms (86.28%), Space: 12.4 MB (78.47%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
@@ -5,10 +5,9 @@ class Solution(object):
         :type high: int
         :rtype: int
         """
-        dif = high - low
         if high % 2 == 0 and low % 2 == 0:
-            return dif/ 2
+            return (high - low)/ 2
         else:
-            return  dif / 2 + 1    
+            return  (high - low) / 2 + 1    
         
         
