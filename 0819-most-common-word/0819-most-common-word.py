226343623286437618a8adb6c16385efb44c5201
AbdBasith17 <abzit377@gmail.com>
2025-12-04 11:45:47 +0530

    Time: 5 ms (40.23%), Space: 12.3 MB (99.61%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def mostCommonWord(self, paragraph, banned):
+        """
+        :type paragraph: str
+        :type banned: List[str]
+        :rtype: str
+        """
+        p = "".join([ch if ch.isalpha() else " " for ch in paragraph])
+        s = p.split()
+        if banned == []:
+            s = [w.lower() for w in s] 
+        elif len(banned )> 1:    
+            s = [w.lower() for w in s if w.lower() not in banned]
+        else:
+            s = [ w.lower() for w in s if w.lower() != banned[0]]    
+
+        count = [s.count(x) for x in s ]
+        return s[count.index(max(count))]
