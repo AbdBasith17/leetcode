9897655ab3ed817a75dc908a7f1a22d0631113a3
AbdBasith17 <abzit377@gmail.com>
2025-12-04 11:53:27 +0530

    Time: 7 ms (33.98%), Space: 12.6 MB (22.07%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.py
+++ 0819-most-common-word/0819-most-common-word.py
@@ -6,9 +6,7 @@ class Solution(object):
         :rtype: str
         """
         p = "".join([ch if ch.isalpha() else " " for ch in paragraph])
-
         s = [w.lower() for w in p.split()]
         s = [w for w in s if w not in banned]    
-
         count = [s.count(x) for x in s ]
         return s[count.index(max(count))]
