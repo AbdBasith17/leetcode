b7703415df5fb107034df4d0f4e4de707cf89663
AbdBasith17 <abzit377@gmail.com>
2026-01-10 13:45:39 +0530

    Time: 1545 ms (25.88%), Space: 12.8 MB (50.59%) - LeetHub

--- 2843-count-symmetric-integers/2843-count-symmetric-integers.py
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.py
@@ -7,12 +7,14 @@ class Solution(object):
         """
         count = 0
         for i in range(low,high+1):
-            if len(str(i)) % 2 == 0:
-                s = str(i)
-                l = int(len(s)/2)
-                s1 = s[:l]
-                s2 = s[l:]
-                if sum(int(x) for x in s1) == sum(int(x) for x in s2):
-                    count += 1
+            s = str(i)
+            n = len(s)
+            if n % 2 != 0:
+                continue 
+            l = n // 2
+            s1 = s[:l]
+            s2 = s[l:]
+            if sum(int(x) for x in s1) == sum(int(x) for x in s2):
+                count += 1
         return count           
                 
