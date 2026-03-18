34388be4517a4cb4ae14ec10b1801013ab332a48
AbdBasith17 <abzit377@gmail.com>
2026-03-18 11:12:26 +0530

    Time: 2 ms (29.27%), Space: 12.3 MB (79.17%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.py
+++ 2427-number-of-common-factors/2427-number-of-common-factors.py
@@ -5,9 +5,10 @@ class Solution(object):
         :type b: int
         :rtype: int
         """
-        n = max(a,b)
-        li = []
+        n = min(a,b)
+        count = 0
         for i in range(1,n+1):
             if a % i ==0 and b %i ==0:
-                li.append(i)
-        return len(li)
+                count += 1
+        return count
+        
