17595d32ff2ac8631dd95a10345dc1549f947b0e
AbdBasith17 <abzit377@gmail.com>
2026-05-18 18:45:21 +0530

    Time: 0 ms (100%), Space: 12.4 MB (17.25%) - LeetHub

---
+++ 3668-restore-finishing-order/3668-restore-finishing-order.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def recoverOrder(self, order, friends):
+        """
+        :type order: List[int]
+        :type friends: List[int]
+        :rtype: List[int]
+        """
+        
+        
+        order = [ x for x in order if x in friends]
+        return order
